3b72c74a6d30443a1f731cfef9fc16a5e5381b65
Maheswari Kumar <ksrimahi@gmail.com>
2024-08-15 22:45:28 +0530

    Done

--- src/navBar.js
+++ src/navBar.js
@@ -1,7 +1,19 @@
 export default function NavBar() {
     return (
         <>
-        <h1>Hello</h1>
+        <div className="bg-pink-300 fixed w-full h-16">
+            <div className="flex items-center pl-2 pt-1 gap-2">
+                <img className="rounded-full" width="48" height="48" src="https://img.icons8.com/external-filled-outline-02-chattapat-/64/external-skateboard-sales-filled-outline-02-chattapat-.png" alt="external-skateboard-sales-filled-outline-02-chattapat-"/>
+                <nav className="trend text-3xl">t</nav>
+                <nav className="trend text-3xl">r</nav>
+                <nav className="trend text-3xl">e</nav>
+                <nav className="trend text-3xl">n</nav>
+                <nav className="trend text-3xl">d</nav>
+                <nav className="trend text-3xl">i</nav>
+                <nav className="trend text-3xl">f</nav>
+                <nav className="trend text-3xl">y</nav>
+            </div>
+        </div>
         </>
     )
 }
